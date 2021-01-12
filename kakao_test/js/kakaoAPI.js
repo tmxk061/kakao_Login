bd05db2b1d0a1c6bea6fd512b21233ccb438a639
tmxk061 <48059518+tmxk061@users.noreply.github.com>
2021-01-12 13:34:32 +0900

    분활

---
+++ kakao_test/js/kakaoAPI.js
@@ -0,0 +1,30 @@
+
+function loginWithKakao() {
+    Kakao.Auth.login({
+      success: function(authObj) {
+        alert(JSON.stringify(authObj));
+        Kakao.API.request({
+              url: '/v2/user/me',
+              success: function(response) {
+                  console.log(response);
+              },
+              fail: function(error) {
+                  console.log(error);
+              }
+          });
+      },
+      fail: function(err) {
+        alert(JSON.stringify(err))
+      },
+    })
+  }
+
+  function LogoutTest(){
+    if (!Kakao.Auth.getAccessToken()) {
+    console.log('Not logged in.');
+    return;
+    }
+    Kakao.Auth.logout(function() {
+    console.log(Kakao.Auth.getAccessToken());
+    });
+  }
