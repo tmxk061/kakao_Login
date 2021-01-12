c4f68bfa56a53a26939ce617aa780a3e19eb4266
tmxk061 <48059518+tmxk061@users.noreply.github.com>
2021-01-12 18:01:01 +0900

    134141

--- kakao_test/js/kakaoAPI.js
+++ kakao_test/js/kakaoAPI.js
@@ -51,36 +51,11 @@ function loginWithKakao() {
   }
 
   function MessageTest(){
-    Kakao.Link.sendDefault({
-        objectType: 'feed',
-        content: {
-          title: '디저트 사진',
-          description: '아메리카노, 빵, 케익',
-          imageUrl:
-            'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
-          link: {
-            mobileWebUrl: 'https://developers.kakao.com',
-            androidExecParams: 'test',
-          },
-        },
-        social: {
-          likeCount: 10,
-          commentCount: 20,
-          sharedCount: 30,
-        },
-        buttons: [
-          {
-            title: '웹으로 이동',
-            link: {
-              mobileWebUrl: 'https://developers.kakao.com',
-            },
-          },
-          {
-            title: '앱으로 이동',
-            link: {
-              mobileWebUrl: 'https://developers.kakao.com',
-            },
-          },
-        ]
+    Kakao.Link.sendCustom({
+        templateId: 44532 ,
+        templateArgs: {
+          'title': '제목 영역입니다.',
+          'description': '설명 영역입니다.'
+        }
       });
   }
