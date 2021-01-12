2d174a34399ac4a52289d0235a0ba1aba010db26
tmxk061 <48059518+tmxk061@users.noreply.github.com>
2021-01-12 14:54:40 +0900

    123124124

--- kakao_test/js/kakaoAPI.js
+++ kakao_test/js/kakaoAPI.js
@@ -1,3 +1,8 @@
+// SDK를 초기화 합니다. 사용할 앱의 JavaScript 키를 설정해 주세요.
+Kakao.init('08f9c3456cd84f0e93a2f99a212dc02a');
+// SDK 초기화 여부를 판단합니다.
+console.log(Kakao.isInitialized());
+
 
 function loginWithKakao() {
     Kakao.Auth.login({
