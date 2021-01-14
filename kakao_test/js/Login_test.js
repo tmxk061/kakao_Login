4c4413de767f4f8e9c76052942a267be42698530
tmxk061 <48059518+tmxk061@users.noreply.github.com>
2021-01-14 14:59:19 +0900

    1234124

--- kakao_test/js/Login_test.js
+++ kakao_test/js/Login_test.js
@@ -2,8 +2,31 @@
 
 var save_id = "1592528927";
 
-function LoginRogic(){
-    var id = loginWithKakao();
+
+function loginWithKakao_main() {
+    Kakao.Auth.login({
+      success: function(authObj) {
+        //alert(JSON.stringify(authObj));
+        Kakao.API.request({
+              url: '/v2/user/me',
+              success: function(response) {
+                  //console.log(response);
+                  //console.log(response.id);
+                  //alert(response.id);
+                  LoginCheck(response.id);
+              },
+              fail: function(error) {
+                  console.log(error);
+              }
+          });
+      },
+      fail: function(err) {
+        alert(JSON.stringify(err))
+      },
+    })
+  }
+
+  function LoginCheck(id){
     console.log(id);
     if(id == save_id)
     {
