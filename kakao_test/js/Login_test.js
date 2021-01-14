f2dd0b9fabb84be1a91a6545eb0887b2a2d250b1
tmxk061 <48059518+tmxk061@users.noreply.github.com>
2021-01-14 16:43:37 +0900

    124211

--- kakao_test/js/Login_test.js
+++ kakao_test/js/Login_test.js
@@ -2,6 +2,30 @@
 
 const save_id_DB = ["1592528927","1592738539"];
 
+//회원가입 과정
+function loginWithKakao_Account() {
+    Kakao.Auth.login({
+      success: function(authObj) {
+        //alert(JSON.stringify(authObj));
+        Kakao.API.request({
+              url: '/v2/user/me',
+              success: function(response) {
+                  //console.log(response);
+                  //console.log(response.id);
+                  alert(response.id + "를 DB에 저장");
+                  //LoginCheckRogic(response.id);
+              },
+              fail: function(error) {
+                  console.log(error);
+              }
+          });
+      },
+      fail: function(err) {
+        alert(JSON.stringify(err))
+      },
+    })
+  }
+
 //로그인 과정
 function loginWithKakao_main() {
     Kakao.Auth.login({
@@ -43,7 +67,6 @@ function loginWithKakao_main() {
             console.log(save_id_DB[item], sid);
             if(save_id_DB[item] == sid)
             {
-                console.log("까꿍");
                 return true;
             }
         }
